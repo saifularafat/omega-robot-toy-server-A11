8ee6861524ec77631b48441cddf2b2815bfa833a
saifularafat <saifularafat.info@gmail.com>
2023-05-19 22:35:25 +0600

    mongodb database added and robot products DB add in get the local server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -8,7 +10,43 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/', (req,  res) => {
+
+const uri = `mongodb+srv://${process.env.DATA_USER}:${process.env.DATA_PASS}@cluster0.guqonkt.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const robotCollection = client.db('kidsRobot').collection('robotProducts')
+
+        app.get('/robotProducts', async(req, res) => {
+            const cursor = robotCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Kids Omega Robot Server You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('This is Omega Robot Server site..')
 })
 
